969aa6a6b232dcc50f0393fc186cef61815275ac
WiggleWoggle <sulvidaurri@gmail.com>
2024-09-27 10:05:31 -0500

    search bar kinda works - allows dupes still so needa fix that - spaces still cause issues (will fix) - display is constantly updating now

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace RecipeOrganizer
     public partial class Form1 : Form
     {
 
+        private List<Recipe> bookmarkedRecipes = new List<Recipe>();
         private List<Recipe> displayedRecipes = new List<Recipe>();
 
         public Form1()
@@ -25,25 +26,84 @@ namespace RecipeOrganizer
         private void button1_Click(object sender, EventArgs e)
         {
 
-            Recipe createdRecipe = new Recipe("Recipe Name");
+            Random random = new Random();
+
+            Recipe createdRecipe = new Recipe("Recipe Name " + random.Next(1, 101));
             createdRecipe.addRecipeTag("TEST");
             createdRecipe.addRecipeTag("TEST2");
             createdRecipe.addRecipeTag("TEST3");
 
-            RecipeManager.addRecipe(createdRecipe);
+            bookmarkedRecipes.Add(createdRecipe);
+            displayedRecipes.Add(createdRecipe);
 
-            RecipeLayoutPanel.Controls.Add(createPanel(createdRecipe));
+            displayRecipes();
+        }
 
-            TabPage createdTab = new TabPage(createdRecipe.getName());
+        private void displayRecipes()
+        {
 
-            RecipeTabControl.TabPages.Add(createdTab);
+            foreach (Recipe recipe in displayedRecipes)
+            {
+                buildRecipe(recipe);
+            }
+        }
+
+        private void clearRecipes()
+        {
+
+            RecipeLayoutPanel.Controls.Clear();
+        }
+
+        private void resetDisplayedToBookmarked()
+        {
+
+            displayedRecipes = bookmarkedRecipes;
+        }
+
+        private void buildRecipe(Recipe recipe)
+        {
+
+            RecipeManager.addRecipe(recipe);
+
+            RecipeLayoutPanel.Controls.Add(createPanel(recipe));
+
+
+            createPanel(recipe);
 
             updateNoBookmarkLabel();
         }
 
         //Search Bar Functionality
 
-            createPanel(recipe);
+        private void SearchInputText_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                clearRecipes();
+                updateDisplayedRecipes(textBox.Text);
+                displayRecipes();
+            } else
+            {
+                resetDisplayedToBookmarked();
+            }
+        }
+
+        private void updateDisplayedRecipes(String textboxString)
+        {
+
+            List<Recipe> temp = new List<Recipe>();
+
+            foreach (Recipe recipe in bookmarkedRecipes)
+            {
+
+                if (recipe.getName().Contains(textboxString)) {
+
+                    temp.Add(recipe);
+                }
+            }
+
+            displayedRecipes = temp;
         }
 
         //UI Recipe Updating/Creation
@@ -59,6 +119,7 @@ namespace RecipeOrganizer
 
             newPanel.Controls.Add(createTitle(recipe));
             newPanel.Controls.Add(createTags(recipe));
+            newPanel.Controls.Add(bookmarkLabel(recipe));
 
             return newPanel;
         }
@@ -70,18 +131,6 @@ namespace RecipeOrganizer
             label.Text = recipe.getName();
             label.Font = new Font("Microsoft Sans Serif", 14);
             label.Size = new Size(300, 24);
-
-            return label;
-        }
-
-        private Label createDescription(Recipe recipe)
-        {
-
-            Label label = new Label();
-            label.Text = recipe.getDescription();
-            label.Font = new Font("Microsoft Sans Serif", 9);
-            label.Size = new Size(300, 24);
-            label.Location = new Point(2, 25);
 
             return label;
         }
@@ -107,5 +156,58 @@ namespace RecipeOrganizer
 
             return label;
         }
-    }       
+
+        private PictureBox bookmarkLabel(Recipe recipe)
+        {
+
+            PictureBox image = new PictureBox();
+            image.Image = Resources.bookmark_icon;
+            image.SizeMode = PictureBoxSizeMode.StretchImage;
+            image.Size = new Size(20, 18);
+            image.Location = new Point(475, 5);
+
+            return image;
+        }
+
+        private void updateNoBookmarkLabel()
+        {
+
+            if (RecipeManager.recipeSize() > 0)
+            {
+                NoBookmarksLabel.Visible = false;
+            }
+            else
+            {
+                NoBookmarksLabel.Visible = true;
+            }
+        }
+
+        //SQL Server Methods
+
+        private void SQLserver()
+        {
+            string connectionString = @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true";
+
+            string createDbQuery = "CREATE DATABASE recipies";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(createDbQuery, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                        System.Diagnostics.Debug.WriteLine("LocalDB database 'recipie' created successfully");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
 }
