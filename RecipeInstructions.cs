f92d62c707c37c0c7af58c76513d4f2104a818a6
WiggleWoggle <sulvidaurri@gmail.com>
2024-10-16 11:00:27 -0500

    4 completed recipes

---
+++ RecipeInstructions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeOrganizer
+{
+    internal class RecipeInstructions
+    {
+        public static List<String> bulgogi()
+        {
+            return new List<String> { 
+                "Gather all ingredients.", 
+                "Whisk soy sauce, white and light green parts of green onions, yellow onion, sugar, garlic, sesame seeds, sesame oil, red pepper flakes, ginger, and black pepper together in a large glass or ceramic bowl.", 
+                "Add steak slices and toss to evenly coat. Cover the bowl with plastic wrap and marinate in the refrigerator for 1 hour, or up to 1 day.", 
+                "Heat a wok or large skillet over medium heat. Working in batches, cook and stir steak and marinade together in the hot skillet, adding honey to caramelize the steak, until steak is cooked through, about 5 minutes.",
+                "Garnish bulgogi with dark green parts of green onions." };
+        }
+
+        public static List<String> bibimbap()
+        {
+            return new List<String> { "Stir the cucumber pieces and gochujang together in a bowl.", 
+                "Cook the spinach until bright green and wilted. ", 
+                "Drain the spinach, squeeze out as much moisture as possible, and set aside. ", 
+                "Cook the carrots until soft, then stir in the cucumber mixture and red pepper flakes.", 
+                "Brown the beef in a different skillet and set aside. ", 
+                "Fry the eggs until the yolks are runny, but whites are firm.", 
+                "Top each serving of rice with the spinach mixture, beef, and the cucumber mixture. ", 
+                "Place an egg atop each serving, then drizzle with sesame oil, sesame seeds, and gochujang (if desired). " };
+        }
+
+        public static List<String> tteokbokki()
+        {
+            return new List<String> {
+                "Rinse frozen rice cakes (2 cups worth) in water. Then drain and set aside.",
+                "Then cut 2 sheets of fishcake (eomuk) into rectangular pieces.",
+                "Take out a cheese grater and quickly grate a small piece of whole onion - we'll only need 1 Tablespoon of Grated Onion for our Sauce.",
+                "Take out a pot and add in the following: Anchovy Dashida (1 Tablespoon), Grated Onion (1 Tablespoon), Water (½ Cup), Gochujang (1 Tablespoon), Fine Gochugaru (1 Tablespoon), White Sugar (1.5 Tablespoons), Maesil Plum Extract Syrup (1 Tablespoon), Miwon (aka MSG) (1 teaspoon!)",
+                "Give it all a good stir - so it blends together",
+                "Place pot on a low heat. Start your timer right after you place the pot on the heat - we want 15 minutes!",
+                "Important: Make sure the heat is on LOW heat - we want a GENTLE simmer (please reference video). NOT a hard boil!",
+                "During the 15 minutes, stir the pot from time-to-time, to make sure it doesn't burn.",
+                "After 15 minutes, the sauce will have thickened - looking like a runny gochujang sauce.",
+                "Add rice cakes and eomuk pieces into a pot.",
+                "Then add-in 1.5 cups of water (~350ml).",
+                "Next, add in all of the Tteokbokki Sauce. Use some water from the Tteokbokki pot and scoop it into your sauce pot - to make sure you get all of the Tteokbokki Sauce out!",
+                "Bring the pot to boil. Once it's boiling, set a timer for 10 minutes.",
+                "Stir it from time-to-time.",
+                "After 10 minutes, you'll be left with one of the most delicious Tteokbokkis!",
+                "Turn off heat. Serve and enjoy life "};
+        }
+
+        public static List<String> baozi()
+        {
+            return new List<String> {
+                "In a large mixing bowl or mixer with a dough hook attachment, completely dissolve yeast and sugar in lukewarm water. Wait 10-15 minutes to let the yeast become active and foam up.",
+                "Add in the flour ½ cup at a time and knead for about 15 minutes. Adjust the last ½ cup flour accordingly. The dough should be soft––not sticky and not too firm. Once the dough is even and smooth, form it into a ball. Cover it with a damp kitchen towel and let it proof one hour in a warm place. (If kneading by hand, knead the dough until it’s as smooth as a baby’s butt!) While it’s proofing, make the filling.",
+                "Add the ground meat to a large mixing bowl and stir in 3 tablespoons water until well incorporated.",
+                "Now preheat the wok or a cast iron pan until it starts to smoke lightly. Add in 3 tablespoons oil, along with the minced ginger and diced onion. Cook over medium heat until the onions soften. Add in the ground pork and turn up the heat, stirring to break up any large chunks. Cook until all the pork turns pale and opaque––there is no need to brown or crisp the meat.",
+                "Add wine, dark soy sauce, sweet bean sauce, ground bean sauce, oyster sauce, sugar, white pepper, and sesame oil. With the heat on high, stir everything together until well-combined. Taste the filling and adjust seasoning if needed.",
+                "Cook for a couple of more minutes to cook off any remaining liquid. Stir in the cornstarch and water mixture, allowing everything to bubble together for 30 seconds to a minute. Turn off the heat and let the filling cool uncovered. After the filling has cooled, mix in the chopped scallions.",
+                "After the dough has finished proofing, turn it out onto a clean surface dusted with flour. Knead for 2 minutes to get rid of any air pockets. Weigh and divide the dough into 20 equal pieces (each should weigh about 48g-50g).",
+                "Take each dough ball, and with a rolling pin, roll it from the edge towards the center, without actually rolling the center of the dough. The goal is to flatten the dough into a round circle with a thinner edge and thicker center. The difference in thickness should be about 1:2. Add some filling to the center.",
+                "You can start with a smaller amount of filling until you get the hang of the folding/pleating. The buns are folded with one hand holding the skin and filling, and the other hand pleating the edges of the dough disk like an accordion. As you fold, the goal is to make it all the way around the circle, until you’ve sealed it at the top. You’ll be making about 10-15 folds. Lay the buns on a small piece of parchment paper and put it directly on your steaming rack. Repeat until all are assembled. Let the buns proof (covered) for another 15 minutes before steaming.",
+                "If you’d rather not pleat the buns at all, simply pinch the edges of dough around the filling and pinch to seal tightly. Then simply flip the buns seam side down, cover, and proof for 15 minutes.",
+                "With the buns in the steamer, start with cold water and turn on the heat to medium. Set the timer to 15 minutes.",
+                "After 15 minutes, turn off the heat, and keep the lid on. Let the buns “rest” for 5 minutes before opening the lid. Missing this step will cause the buns to collapse.",
+                "Once the buns are steamed and cooled completely, pack them in an airtight container. They can be refrigerated or frozen.",
+                "Out of the refrigerator, simply microwave the buns for 1-2 minutes. Alternatively, re-steam them for 5 minutes. If re-heating frozen buns, there’s no need to thaw beforehand. Simply steam them for 8 minutes."};
+        }
+
+        public static List<String> f()
+        {
+            return new List<String> {
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+        }
+
+        public static List<String> g()
+        {
+            return new List<String> {
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+        }
+    }
+}
